4d5d60291d21766c3d357bac8ee6c7801c7df023
Bryan Yee <yee.bryan.c@gmail.com>
2016-07-27 10:48:25 -0700

    Split function for reqesting and displaying movie data

--- Javascript/app.js
+++ Javascript/app.js
@@ -52,14 +52,14 @@ function showResult(result) {
   let resultDiv = document.createElement('div');
   let colorClass = (resultsIndex % 2 === 0 ) ? 'bc-light-gray' : 'bc-light-gray2';
 
-  resultDiv.innerHTML = `<div class='results-box ${colorClass}' onclick='getMovieData(${id})'>${title} ${releaseDate}</div>`;
+  resultDiv.innerHTML = `<div class='results-box ${colorClass}' onclick='requestMovieData(${id})'>${title} ${releaseDate}</div>`;
   resultsContainer.appendChild(resultDiv);
 
   resultsIndex++;
 }
 
-//Request and display detailed data for a specified movie
-function getMovieData(id) {
+//Request detailed data for a specified movie
+function requestMovieData(id) {
   let movieData;
   let url = `http://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
 
@@ -70,21 +70,26 @@ function getMovieData(id) {
 
   request.onreadystatechange = function() {
     if (request.readyState === 4) {
-      movieData = JSON.parse(request.responseText);
-      preFormatResults('movieData');
-      let title = movieData.title || 'No title available';
-      let overview = movieData.overview || 'No overview available.';
-      let posterPath = `http://image.tmdb.org/t/p/w500${movieData.poster_path}`;
-      let releaseYear = movieData.release_date ? `(${movieData.release_date.slice(0,4)})` : 'No release date available';
-      let rating = movieData.vote_average ? `${movieData.vote_average} / 10` : 'No rating available';
-
-      resultsContainer.innerHTML =  movieData.poster_path ? `<img src='${posterPath}'>` : '';
-      resultsContainer.innerHTML += `<h1>${title}</h1>`;
-      resultsContainer.innerHTML += `<span class='color-dark-gray'>${releaseYear}</span>`;
-      resultsContainer.innerHTML += `<div id='rating'>${rating}</div><br><br>`;
-      resultsContainer.innerHTML += `<span>${overview}</span>`;
+      displayMovieData(request);
     }
   };
 
   request.send();
-}//end of search
+}
+
+//Display detailed data for a specified movie
+function displayMovieData(request){
+  movieData = JSON.parse(request.responseText);
+  preFormatResults('movieData');
+  let title = movieData.title || 'No title available';
+  let overview = movieData.overview || 'No overview available.';
+  let posterPath = `http://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+  let releaseYear = movieData.release_date ? `(${movieData.release_date.slice(0,4)})` : 'No release date available';
+  let rating = movieData.vote_average ? `${movieData.vote_average} / 10` : 'No rating available';
+
+  resultsContainer.innerHTML =  movieData.poster_path ? `<img src='${posterPath}'>` : '';
+  resultsContainer.innerHTML += `<h1>${title}</h1>`;
+  resultsContainer.innerHTML += `<span class='color-dark-gray'>${releaseYear}</span>`;
+  resultsContainer.innerHTML += `<div id='rating'>${rating}</div><br><br>`;
+  resultsContainer.innerHTML += `<span>${overview}</span>`;
+}
