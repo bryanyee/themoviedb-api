d7c46a967af3c0fee4d79a2a57f4d9b3f9462ddf
Bryan Yee <yee.bryan.c@gmail.com>
2016-07-26 17:28:06 -0700

    Updated layout, and added Search box

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,24 +1,27 @@
  
-var request = new XMLHttpRequest();
-var key = 'd396c67805d03e62ce8fb5ca26923338';
+var key = '<INSERT_YOUR_API_KEY>';
 var movieData;
-var mainContainer = document.getElementById('main-container');
+var resultsContainer = document.getElementById('results-container');
 
-request.open('GET', 'http://api.themoviedb.org/3/movie/209112?api_key=' + key);
+function movieSearch() {
+  var request = new XMLHttpRequest();  
 
-request.setRequestHeader('Accept', 'application/json');
+  request.open('GET', 'http://api.themoviedb.org/3/movie/209112?api_key=' + key);  
 
-request.onreadystatechange = function (response) {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    movieData = JSON.parse(this.responseText);
-    console.log(movieData.original_title);
-    console.log(movieData.overview);
+  request.setRequestHeader('Accept', 'application/json');  
 
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      console.log('Status:', request.status);
+      console.log('Headers:', request.getAllResponseHeaders());
+      movieData = JSON.parse(request.responseText);
+      console.log(movieData.original_title);
+      console.log(movieData.overview);  
+  
 
-    mainContainer.innerHTML = movieData.original_title + '<br>' + movieData.overview;
-  }
-};
+      resultsContainer.innerHTML = movieData.original_title + '<br>' + movieData.overview;
+    }
+  };
 
-request.send();
+  request.send();
+}//end of search
