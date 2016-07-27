ea31c54b33ae69bf16260a26bf9eb0fee8726042
Bryan Yee <yee.bryan.c@gmail.com>
2016-07-27 10:41:31 -0700

    Added more movie data

--- Javascript/app.js
+++ Javascript/app.js
@@ -4,7 +4,7 @@ const resultsContainer = document.getElementById('results-container');
 const movieSearchBox = document.getElementById('movieSearchBox');
 let resultsIndex = 0;
 
-//Create list of movie results based on the search box
+//Request and display list of movie results based on the search box
 function searchForMovies() {
   //Prepare for AJAX request
   let queryValue = movieSearchBox.value;
@@ -13,9 +13,7 @@ function searchForMovies() {
   let searchResults;
 
   //Reset resultsContainer
-  resultsContainer.innerHTML = `<h3>Results for "${queryValue}"</h3><br><br>`;
-  resultsContainer.style.display = 'block';
-  resultsIndex = 0;
+  preFormatResults('resultsList', queryValue);
 
   //AJAX request to search for movies by title
   let request = new XMLHttpRequest();
@@ -32,20 +30,35 @@ function searchForMovies() {
   request.send();
 }
 
+function preFormatResults(option, queryValue) {
+  if (option === 'resultsList') {
+    resultsContainer.innerHTML = `<h3>Results for "${queryValue}"</h3><br><br>`;
+    resultsContainer.style.display = 'block';
+    resultsContainer.style.backgroundColor = 'white';
+    resultsIndex = 0;
+  }
+  else if (option === 'movieData') {
+    resultsContainer.innerHTML = "";
+    resultsContainer.style.backgroundColor = '#e9e9e9';
+  }
+}
+
 //Display summary data for a movie search result
 function showResult(result) {
   let title = result.title;
   let id = result.id;
+  let releaseDate = `<span class='year'>(${result.release_date.slice(0,4)})</span>` || `<span class='year'>(release date unavailable)</span>`;
+
   let resultDiv = document.createElement('div');
   let colorClass = (resultsIndex % 2 === 0 ) ? 'bc-light-gray' : 'bc-light-gray2';
 
-  resultDiv.innerHTML = `<div class='results-box ${colorClass}' onclick='getMovieData(${id})'>${title}</div>`;
+  resultDiv.innerHTML = `<div class='results-box ${colorClass}' onclick='getMovieData(${id})'>${title} ${releaseDate}</div>`;
   resultsContainer.appendChild(resultDiv);
 
   resultsIndex++;
 }
 
-//Display detailed data for a specified movie
+//Request and display detailed data for a specified movie
 function getMovieData(id) {
   let movieData;
   let url = `http://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
@@ -58,10 +71,18 @@ function getMovieData(id) {
   request.onreadystatechange = function() {
     if (request.readyState === 4) {
       movieData = JSON.parse(request.responseText);
-      let title = movieData.title;
-      let overview = movieData.overview || "No overview available.";
+      preFormatResults('movieData');
+      let title = movieData.title || 'No title available';
+      let overview = movieData.overview || 'No overview available.';
+      let posterPath = `http://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+      let releaseYear = movieData.release_date ? `(${movieData.release_date.slice(0,4)})` : 'No release date available';
+      let rating = movieData.vote_average ? `${movieData.vote_average} / 10` : 'No rating available';
 
-      resultsContainer.innerHTML = `${title}<br>${overview}`;
+      resultsContainer.innerHTML =  movieData.poster_path ? `<img src='${posterPath}'>` : '';
+      resultsContainer.innerHTML += `<h1>${title}</h1>`;
+      resultsContainer.innerHTML += `<span class='color-dark-gray'>${releaseYear}</span>`;
+      resultsContainer.innerHTML += `<div id='rating'>${rating}</div><br><br>`;
+      resultsContainer.innerHTML += `<span>${overview}</span>`;
     }
   };
 
